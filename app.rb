edf7512c366014fef9eac6c339848a8ab6a66151
Romano Livi <romano_livi@outlook.com>
2021-01-26 23:26:36 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,19 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
-
+  get '/newteam' do 
+    erb :newteam
+  end
+  
+  post '/newteam' do 
+    @team_name = params[:team]
+    @coach = params[:coach]
+    @point = params[:pg]
+    @shoot = params[:sg]
+    @small = params[:sf]
+    @power = params[:pf]
+    @center = params[:c]
+    erb:newteam
+  end
 
 end
